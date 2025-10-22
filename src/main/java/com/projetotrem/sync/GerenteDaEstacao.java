71a589c639b53aa5505a82d72105555f416b2e96
matheus-sortech <matheus.almeida@sortech.com.br>
2025-10-22 19:29:10 -0300

    feat: get e set dos métodos dos empacotadores: getStatusDosEmpacotadores, getQuantidadeDeCaixasDepositadas e setStatusDosEmpacotadores

--- src/main/java/com/projetotrem/sync/GerenteDaEstacao.java
+++ src/main/java/com/projetotrem/sync/GerenteDaEstacao.java
@@ -1,5 +1,7 @@
 package com.projetotrem.sync;
 
+import com.projetotrem.model.Empacotador;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Semaphore;
@@ -38,9 +40,46 @@ public class GerenteDaEstacao {
 
     public void setStatusDoTrem(String status) {return;}
 
-    public int getQuantidadeDeCaixasDepositadas() {return 1;}
+    public void setStatusDosEmpacotadores(int idEmpacotador, String status) {
+        try {
+            mutex.acquire();
+            this.statusDosEmpacotadores.put(idEmpacotador, status);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            mutex.release();
+        }
+    }
+
+    public int getQuantidadeDeCaixasDepositadas() {
+        int count = 0;
+
+        try {
+            mutex.acquire();
+            count = this.quantidadeDeCaixasDepositadas;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            mutex.release();
+        }
+        return count;
+    }
 
     public String getStatusDoTrem() {return "";}
 
-    public Map<Integer, String> getStatusDosEmpacotadores() {return null;}
+    public Map<Integer, String> getStatusDosEmpacotadores() {
+        Map<Integer, String> statusDosEmpacotadoresCopia;
+        try {
+            mutex.acquire();
+
+            statusDosEmpacotadoresCopia = new HashMap<>(this.statusDosEmpacotadores);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return new HashMap<>();
+        } finally {
+            mutex.release();
+        }
+        return statusDosEmpacotadoresCopia;
+    }
+
 }
