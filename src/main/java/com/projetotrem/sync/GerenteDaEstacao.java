98f1eea1f01ef900e375ac243d533abff963021e
Nicolas Pinheiro <nicolaspinheirogomes3@gmail.com>
2025-10-28 04:21:04 -0300

    feat: add train feature, support multiple Packers, and add debug logging (application can now run from main)

--- src/main/java/com/projetotrem/sync/GerenteDaEstacao.java
+++ src/main/java/com/projetotrem/sync/GerenteDaEstacao.java
@@ -1,7 +1,11 @@
 package com.projetotrem.sync;
 
+import com.projetotrem.model.Empacotador;
+import com.projetotrem.model.Trem;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
 public class GerenteDaEstacao {
@@ -21,7 +25,7 @@ public class GerenteDaEstacao {
         this.capacidadeDoDeposito = capacidadeDoDeposito;
 
         this.quantidadeDeCaixasDepositadas = 0;
-        this.statusDoTrem = "Parado";
+        this.statusDoTrem = "Esperando carregamento";
         this.statusDosEmpacotadores = new HashMap<>();
 
         this.mutex = new Semaphore(1);
@@ -47,10 +51,35 @@ public class GerenteDaEstacao {
        quantidadeDeCaixasNoDeposito.release();
     }
 
-    public void carregaTrem() throws InterruptedException {return;}
+    public void carregaTrem() throws InterruptedException {
+        setStatusDoTrem("Esperando carregamento!");
+        quantidadeDeCaixasNoDeposito.acquire(capacidadeDoTrem);
+        mutex.acquire();
+        try {
+            _unsafe_setStatusDoTrem("Entregando");
+
+            quantidadeDeCaixasDepositadas -= capacidadeDoTrem;
+
+            System.out.println("[+] Trem saiu para entrega");
+        } finally {
+            mutex.release();
+        }
+
+        espacosDisponiveisNoDeposito.release(capacidadeDoTrem);
+    }
 
 
-    public void setStatusDoTrem(String status) {return;}
+    public void setStatusDoTrem(String status) {
+        try {
+            mutex.acquire();
+
+            _unsafe_setStatusDoTrem(status);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            mutex.release();
+        }
+    }
 
     public void setStatusDosEmpacotadores(int idEmpacotador, String status) {
         try {
@@ -68,6 +97,10 @@ public class GerenteDaEstacao {
         this.statusDosEmpacotadores.put(idEmpacotador, status);
     }
 
+    private void _unsafe_setStatusDoTrem(String status) {
+        this.statusDoTrem = status;
+    }
+
     public int getQuantidadeDeCaixasDepositadas() {
         int count = 0;
 
@@ -82,7 +115,19 @@ public class GerenteDaEstacao {
         return count;
     }
 
-    public String getStatusDoTrem() {return "";}
+    public String getStatusDoTrem() {
+        String statusDoTremCopia;
+        try {
+            mutex.acquire();
+            statusDoTremCopia = this.statusDoTrem;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            statusDoTremCopia = "";
+        } finally {
+            mutex.release();
+        }
+        return statusDoTremCopia;
+    }
 
     public Map<Integer, String> getStatusDosEmpacotadores() {
         Map<Integer, String> statusDosEmpacotadoresCopia;
@@ -99,4 +144,38 @@ public class GerenteDaEstacao {
         return statusDosEmpacotadoresCopia;
     }
 
+    public void createEmpacotador(int idEmpacotador, Scanner sc) {
+        System.out.println("Qual o tempo de empacotamento em segundos deste empacotador?");
+        long tempoEmpacotamento = sc.nextLong();
+        Empacotador empacotador = new Empacotador(this, idEmpacotador, tempoEmpacotamento);
+        Thread t =  new Thread(empacotador);
+        t.start();
+    }
+
+    public void createTrem(int idTrem, Scanner sc) {
+        System.out.println("Qual o tempo de viagem em segundos do trem? Tempo de ir do A para o B");
+        int tempoViagem =  sc.nextInt();
+        Trem trem = new Trem(this, idTrem, tempoViagem);
+        Thread t =  new Thread(trem);
+        t.start();
+    }
+
+    public void getStatusSistema() {
+        while (true) {
+            for (Map.Entry<Integer, String> entry : getStatusDosEmpacotadores().entrySet()) {
+                Integer id = entry.getKey();
+                String status = entry.getValue();
+                System.out.println("Empacotador ID: " + id + " | Status: " + status);
+            }
+
+            System.out.println("Status do Trem: " + getStatusDoTrem());
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
 }
